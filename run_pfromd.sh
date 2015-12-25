83500575d0ced5c5210d7eacbe8fc2e57f5af542
huseinz <zubirhusein@gmail.com>
2015-12-25 06:31:01 -0500

    Reduced pfromd search interval

--- run_pfromd.sh
+++ run_pfromd.sh
@@ -56,7 +56,7 @@ ANCHORPOINTS=`$APOINTPARSE_PATH log_*`
 echo
 echo "Projected anchorpoints: $ANCHORPOINTS"
 
-ls matrices | parallel --eta -j$JOBS "\"$POINTFINDER_PATH\" matrices/{} 500 2500 500 1000000000 $ANCHORPOINTS"
+ls matrices | parallel --eta -j$JOBS "\"$POINTFINDER_PATH\" matrices/{} 250 1250 250 1000000000 $ANCHORPOINTS"
 
 "$BESTPOINTPARSE_PATH" > points.txt
 
